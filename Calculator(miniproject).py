86326a930419682d5decf555c4bfc78096418649
srikanth24git <k.srikanth2407@gmail.com>
2025-12-30 19:39:42 +0530

    Add files via upload

---
+++ Calculator(miniproject).py
@@ -0,0 +1,125 @@
+"""                                                         MINI PROJECT                                            """
+
+"""
+GUI Calculator Application
+A Tkinter-based Desktop Application
+What is Tkinter?
+Tkinter is Python's built-in library for creating Graphical User Interface (GUI)
+applications.
+It provides widgets like Window, Button, Entry, Label, etc.
+It follows an event-driven programming model (code runs when a user clicks something).
+
+Purpose of this program:
+Creates a Calculator GUI 
+Allows user to:
+    Enter numbers and operators
+    Perform arithmetic calculations
+    Clear the input
+Uses python's eval() function to evaluate mathematical expressions.
+
+Key concepts used:
+Concept                 Description
+Tkinter widgets         Tk, Entry, Button
+Event Handling          Button click actions
+Lambda functions        Passing button values
+Grid Layout             Positioning UI elements
+Exception handling      Handling invalid expressions
+
+"""
+import tkinter as tk    #Imports tkinter module tk is an alias for easy access
+
+def press(v):
+    entry.insert(tk.END, v)     #Called when a no. or operator button is clicked, inserts the pressed value at the end of Entry widget
+
+def clear():
+    entry.delete(0, tk.END)     #Clears the character screen, deletes all characters from index 0 to END
+
+def calc():
+    try:
+        result = eval(entry.get())      #entry.get() gets the expression, eval() evaluates the string as an expression
+
+        entry.delete(0, tk.END)
+        entry.insert(0, result)
+
+    except:
+        entry.delete(0, tk.END)
+        entry.insert(0, "INVALID EXPRESSION")       #Handles invalid expressions display the error message instead of crashing
+
+#Main window creation
+root = tk.Tk()      #Creates the main application window
+root.title("Calculator")    #Sets window title
+root.configure(bg="#1e1e1e")    #Sets background color
+root.resizable(False, False)    #makes window non-resizable
+
+#Entry widget (display screen)
+entry = tk.Entry(
+    root,
+    font=("Gothic", 20),
+    bg="#2d2d2d",
+    fg="white",
+    bd=0,
+    justify="right"
+)
+
+"""Text input field
+Acts as calculator display
+Right-aligned for better calculator look"""
+
+entry.grid(row=0, column=0, columnspan=4, padx=12, pady=12, ipady=10)
+"""Places entry at top columnspan=4 makes it stretch across columns"""
+
+#Buttons labels
+buttons = [
+    "7","8","9","/",
+    "4","5","6","*",
+    "1","2","3","-",
+    "0",".","=","+"
+]
+"""Represent calculator button
+stored in list to reduce repetitive code"""
+
+#Dynamic Button Creation
+r=1
+c=0
+"""Rows and column counters for grid layout"""
+
+for b in buttons:
+    cmd = calc if b=="=" else lambda x=b: press(x)
+    """if button is "=" call calc()
+    otherwise, call press() with the button value
+    lambda x=b prevents late binding issue"""
+
+    tk.Button(
+        root,
+        text=b,
+        command=cmd,   #These 3 lines creates a button widget
+        font=("Times New Roman", 15),
+        width=5,
+        height=3,
+        bg="#3333F6" if b in "+-/*" else "#9932ee",
+        fg="white",
+        bd=0
+    ).grid(row=r, column=c, padx=6, pady=6)
+    c+=1
+    if c == 4:
+        r+=1
+        c=0
+        """moves to next row after 4 buttons"""
+
+#Clear Button
+tk.Button(
+    root,
+    text="C",
+    command=clear,
+    font=("Arial", 15),
+    fg="black",
+    bg="#F93333",
+    bd=0,
+    width=22,
+    height=2
+).grid(row=r, column=0, columnspan=4, pady=8)
+
+root.mainloop()     #Keeps the window running 
+
+
+
